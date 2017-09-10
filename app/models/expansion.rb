3be8c51fad5403ce93d4109f02befb1d140df614
Holden Omans <Holden.Omans@optanix.com>
2017-09-10 15:35:56 -0400

    added db seeds for expansions

--- app/models/expansion.rb
+++ app/models/expansion.rb
@@ -1,3 +1,10 @@
 class Expansion < ApplicationRecord
   has_many :monsters
+
+  attribute :release, :datetime
+  attribute :color, :string, default: '#333'
+  attribute :bg_color, :string, default: '#fff'
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+kdm_manager = JSON.parse(File.read(Rails.root.join('vendor', 'data', 'kdm_manager.json'))).deep_symbolize_keys
+cards = JSON.parse(File.read(Rails.root.join('vendor', 'data', 'cards.json')))
+
+kdm_manager[:expansions].each_value do |exp|
+  expansion = {
+    name: exp[:name],
+    released: DateTime.new(2016, 3),
+    color: '#333',
+    bg_color: '#fff'
+  }
+
+  if exp[:flair]
+    expansion[:color] = exp[:flair][:color]
+    expansion[:bg_color] = exp[:flair][:bgcolor]
+  end
+
+  if exp[:released] && exp[:released] =~ /\d+/
+    expansion[:released] = DateTime.strptime(exp[:released], '%s')
+  end
+  # puts expansion
+
+  Expansion.new(expansion).save
+end
