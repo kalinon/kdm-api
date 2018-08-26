1e85ba1a808cc3793989c8683440a60cc4ef5bbe
Holden Omans <holden.omans@gmail.com>
2018-08-26 14:55:33 -0400

    cleaned up methods

--- spec/test_request.rb
+++ spec/test_request.rb
@@ -1,82 +1,100 @@
 module TestRequest
   extend ActiveSupport::Concern
 
-  def subject_klass
+  def _subject_klass
     @subject_klass.constantize
   end
 
-  def subject_key
-    subject_klass.to_s.underscore.to_sym
+  def _subject_key
+    _subject_klass.to_s.underscore.to_sym
   end
 
-  def body_schema
+  def _body_schema
     {
       type: :object,
       properties: {
-        subject_key => obj_schema
+        _subject_key => _obj_schema
       }
     }.deep_symbolize_keys
   end
 
-  def obj_schema
+  def _obj_schema
     {
       type: :object,
-      properties: obj_properties
+      properties: _obj_properties
     }.deep_symbolize_keys
   end
 
-  def obj_properties
+  def _obj_properties
     props = {}
-    subject_klass.attribute_types.each do |key, type|
+    _subject_klass.attribute_types.each do |key, type|
       translated_type = type.class.to_s.demodulize.underscore.to_sym
 
-      puts translated_type.inspect
       # Handle non-swagger types
-      props[key] = case translated_type
-                   when :time_zone_converter
-                     { type: :string }
-                   when :date
-                     { type: :string }
-                   when :enum_type
-                     {
-                       type: :string,
-                       enum: subject_klass.defined_enums[key].keys
-                     }
-                   else
-                     { type: translated_type }
-                   end
+      props[key] = _obj_properties_type(key, translated_type)
     end
     props.deep_symbolize_keys
   end
 
-  def new_body(properties)
+  def _obj_properties_type(key, translated_type)
+    case translated_type
+    when :time_zone_converter
+      { type: :string }
+    when :date
+      { type: :string }
+    when :enum_type
+      {
+        type: :string,
+        enum: _subject_klass.defined_enums[key].keys
+      }
+    else
+      { type: translated_type }
+    end
+  end
+
+  def _new_body(properties)
     new_args = {}
-    properties.each do |key, type|
-      puts "#{key} => #{type}"
-      new_args[key] = case key
-                      when :id
-                        Faker::Number.number(4)
-                      when :name
-                        Faker::Name.name
-                      when :expansion_id
-                        1
-                      end
+    properties.each do |key, _type|
+      new_args[key] = new_object_value(key)
     end
     new_args.delete_if { |_key, value| value.nil? }
-    { subject_key => new_args }.deep_symbolize_keys
+    { _subject_key => new_args }.deep_symbolize_keys
+  end
+
+  def new_object_value(key)
+    case key
+    when :id
+      Faker::Number.number(4)
+    when :name
+      Faker::Name.name
+    when :expansion_id
+      1
+    end
   end
 
   def test_request
     @subject_klass = to_s.demodulize.singularize
 
-    subject_klass = self.subject_klass
-    subject_key = self.subject_key
-    body_schema = self.body_schema
-    obj_schema = self.obj_schema
-    obj_properties = self.obj_properties
-    new_body = self.new_body(obj_properties)
+    args = {
+      subject_klass: _subject_klass,
+      subject_key: _subject_key,
+      body_schema: _body_schema,
+      obj_schema: _obj_schema,
+      obj_properties: _obj_properties,
+      new_body: _new_body(_obj_properties)
+    }
 
-    path "/#{subject_key.to_s.pluralize}" do
+    # Path List
+    test_path_list(args)
+
+    # Path Object
+    test_path_list(args)
+  end
+
+  # Tests GET, POST on object list
+  def test_path_list(args)
+    # Path List
+    path "/#{args[:subject_key].to_s.pluralize}" do
       get summary: 'fetch list' do
         produces 'application/json'
         response 200, description: 'success'
@@ -86,38 +104,41 @@ module TestRequest
         produces 'application/json'
         consumes 'application/json'
 
-        parameter 'body', in: :body, schema: obj_schema
+        parameter 'body', in: :body, schema: args[:obj_schema]
 
-        let(:body) { new_body }
+        let(:body) { args[:new_body] }
 
         response(201, description: 'successfully created') do
           it 'uses the body we passed in' do
             obj = JSON.parse(response.body)
-            expect(obj['name']).to eq(new_body[subject_key][:name])
-            expect(obj['safe_name']).to eq(new_body[subject_key][:name].parameterize)
+            vals = args[:new_body][args[:subject_key]]
+            expect(obj['name']).to eq(vals[:name])
+            expect(obj['safe_name']).to eq(vals[:name].parameterize)
           end
           capture_example
-          schema(obj_schema)
+          schema(args[:obj_schema])
         end
       end
     end
+  end
 
-    # Path Object
-    path "/#{subject_key.to_s.pluralize}/{id}" do
+  # Tests GET, PATCH, PUT, DELETE on object
+  def test_path_object(args)
+    path "/#{args[:subject_key].to_s.pluralize}/{id}" do
       parameter 'id', in: :path, type: :integer
       let(:id) { 1 }
 
       get summary: 'fetch item' do
         produces 'application/json'
-        response 200, description: 'success', schema: obj_schema
+        response 200, description: 'success', schema: args[:obj_schema]
       end
 
       patch summary: 'update item' do
         produces 'application/json'
         consumes 'application/json'
-        parameter 'body', in: :body, schema: body_schema
+        parameter 'body', in: :body, schema: args[:body_schema]
 
-        let(:body) { { subject_key => { name: 'Update Test Patch' } } }
+        let(:body) { { args[:subject_key] => { name: 'Update Test Patch' } } }
 
         response(200, description: 'successfully created') do
           it 'uses the body we passed in' do
@@ -127,16 +148,16 @@ module TestRequest
           end
 
           capture_example
-          schema(obj_schema)
+          schema(args[:obj_schema])
         end
       end
 
       put summary: 'update item' do
         produces 'application/json'
         consumes 'application/json'
-        parameter 'body', in: :body, schema: body_schema
+        parameter 'body', in: :body, schema: args[:body_schema]
 
-        let(:body) { { subject_key => { name: 'Update Test Put' } } }
+        let(:body) { { args[:subject_key] => { name: 'Update Test Put' } } }
 
         response(200, description: 'successfully created') do
           it 'uses the body we passed in' do
@@ -146,7 +167,7 @@ module TestRequest
           end
 
           capture_example
-          schema(obj_schema)
+          schema(args[:obj_schema])
         end
       end
 
